41775af2661fa1c69b2c579bf6c1ce044e0d58ba
omar <omarmaboelenen@gmail.com>
2018-09-26 16:16:02 +0200

    changed view specs

--- spec/views/app/index.html.erb_spec.rb
+++ spec/views/app/index.html.erb_spec.rb
@@ -1,12 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe "app/index.html.erb", type: :view do
-
-  it 'should contain button to add language' do
+  it 'should contain form with input field to language' do
     assign(:dictKeys, [])
     render
-    expect(rendered).to match("<button>")
-    expect(rendered).to match("Add Language")
+    expect(rendered).to have_css("p#language_label")
+    expect(rendered).to have_css("input#language_name")
   end
 
   it 'should show the dictionary form with the keys from locale' do
@@ -15,7 +14,15 @@ RSpec.describe "app/index.html.erb", type: :view do
 
     render
     expect(rendered).to have_css("form#addLanguageForm")
+    expect(rendered).to have_css("input[name=\"#{expected_key}\"]")
+
     expect(rendered).to have_xpath("//p[contains(text(),\"#{expected_key}\")]")
+  end
+
+  it 'should contain a button Add Language' do
+    assign(:dictKeys, [])
+    render
+    expect(rendered).to have_css("input#add_language")
   end
 
   it 'should contain a form that redirect to /add_lang' do
@@ -23,7 +30,7 @@ RSpec.describe "app/index.html.erb", type: :view do
     assign(:dictKeys, [expected_key])
 
     render
-    expect(rendered).to have_css('form[action="/app/create"]')
+    expect(rendered).to have_css('form[action="/app"]')
+    expect(rendered).to have_css('form[method="post"]')
   end
-
 end
