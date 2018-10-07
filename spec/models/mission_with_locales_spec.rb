84b788fa40775551731dcd40233a8be8d39f7384
omar <omarmaboelenen@gmail.com>
2018-10-07 16:01:03 +0200

    refactored mission_with_locales_spec and fixed test setup

--- spec/models/mission_with_locales_spec.rb
+++ spec/models/mission_with_locales_spec.rb
@@ -40,75 +40,62 @@ describe 'Mission returns title and instructions by language' do
     record.duration = 10
     record.title = 'demo_test_title'
     record.instructions = 'go up and down'
-    create_base_yml_file_without_missions( 'en_test')
 
-    I18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
-    I18n.locale = 'en_test'
+    reset_locale 'en_test'
     record.save
 
     yml_hash = YAML.load(File.read(yml_path('en_test')))
     expect(yml_hash['en_test']['missions']["m_#{record.id.to_s}"][:instructions]).to eql('go up and down')
   ensure
-    File.delete(yml_path('en_test')) if File.exists? yml_path('en_test')
+    remove_locale_file'en_test'
   end
 
   it 'reads the title correctly based on locale' do
     ar_title = 'daght'
     en_title = 'pushup'
 
-    # create locale files
-    create_yml_file_for_locale_mission('en_test', 1, en_title, 'exercise')
-
     create_yml_file_for_locale_mission('ar_test', 1, ar_title, 'instructions')
 
-    I18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
-
-    # create new mission
+    reset_locale 'en_test'
     record = Mission.new
+    record.title = en_title
+    record.instructions = 'exercise'
     record.category = '96'
     record.duration = 10
     record.save
 
-    # create en_test
-    I18n.locale = 'en_test'
+    reset_locale 'en_test'
     expect(record.title).to eql(en_title)
 
-    I18n.locale = 'ar_test'
+    reset_locale 'ar_test'
     expect(record.title).to eql(ar_title)
-
   ensure
     remove_locale_file 'en_test'
     remove_locale_file 'ar_test'
   end
 
   it 'reads the instructions correctly based on locale' do
-    ar_instructions = 'exercise'
-    en_instructions = 'tamarin'
-
-    # create locale files
-    create_yml_file_for_locale_mission('en_test', 1, 'en_title', en_instructions)
+    en_instructions = 'exercise'
+    ar_instructions = 'tamarin'
 
     create_yml_file_for_locale_mission('ar_test', 1, 'ar_title', ar_instructions)
 
-    I18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
-
-    # create new mission
+    reset_locale 'en_test'
     record = Mission.new
+    record.title = 'en_title'
+    record.instructions = en_instructions
     record.category = '96'
     record.duration = 10
     record.save
 
-    # create en_test
-    I18n.locale = 'en_test'
+    reset_locale 'en_test'
     expect(record.instructions).to eql(en_instructions)
 
-    I18n.locale = 'ar_test'
+    reset_locale 'ar_test'
     expect(record.instructions).to eql(ar_instructions)
 
   ensure
-    reset_locale 'en_test'
-    reset_locale 'ar_test'
+    remove_locale_file 'en_test'
+    remove_locale_file 'ar_test'
   end
-
-
 end
